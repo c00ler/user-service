a34fdd8c54b2393bf52cad1ace9147f2e5042089
Alexey Venderov <alexey.venderov@revolut.com>
2020-02-23 16:23:52 +0100

    initial commit

---
+++ consumer/src/main/java/com/github/avenderov/consumer/Launcher.java
@@ -0,0 +1,8 @@
+package com.github.avenderov.consumer;
+
+public final class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ consumer/src/test/java/com/github/avenderov/consumer/LauncherTest.java
@@ -0,0 +1,13 @@
+package com.github.avenderov.consumer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LauncherTest {
+
+    @Test
+    void shouldPass() {
+        assertThat(true).isTrue();
+    }
+}
